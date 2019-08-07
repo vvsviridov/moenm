82909b3a11e4918e0896ad160f3b4cec852233ed
Vyacheslav Sviridov <Vyacheslav.Sviridov@tele2.ru>
2019-08-07 16:18:36 +0600

    gsm rename add

--- motest1.py
+++ motest1.py
@@ -6,23 +6,25 @@ Created on 21 nov. 2018
 
 from moenm import MoENM
 
+
 def main():
     with MoENM("", "", "") as moe:
-        
+
         fdn = "SubNetwork=ONRM_ROOT_MO,SubNetwork=RNC1,MeContext=RNC1,ManagedElement=1,RncFunction=1,UtranCell="
-        
+
         print moe.get("{0}Cell1;{0}Cell4".format(fdn), "administrativeState,userLabel", "-t")
         print moe.set("{0}Cell1;{0}Cell4".format(fdn), "--preview", userLabel="test", administrativeState="UNLOCKED")
-        
+
         print moe.get("{0}Cell1;{0}Cell4".format(fdn), "administrativeState,userLabel", "-t")
         print moe.set("{0}Cell1;{0}Cell4".format(fdn), "--preview", administrativeState="LOCKED")
-        
+
         print moe.get("{0}Cell1;{0}Cell4".format(fdn), "administrativeState,userLabel", "-t")
-        
+
         fdn = "SubNetwork=ONRM_ROOT_MO,SubNetwork=Subn1,MeContext=Rbs1,ManagedElement=1,ENodeBFunction=1,EUtranCellFDD=ECell1"
-        
+
         print moe.get(fdn, "earfcndl,earfcnul", "-t")
         print moe.action(fdn, "changeFrequency", "--preview", earfcn="3456")
+
 
 if __name__ == '__main__':
     main()
