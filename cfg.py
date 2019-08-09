7451861486c8456ae885e7016b85d8f513a6d0bc
Vyacheslav Sviridov <Vyacheslav.Sviridov@tele2.ru>
2019-08-09 12:09:06 +0600

    change to universal

--- cfg.py
+++ cfg.py
@@ -1,6 +1,8 @@
 """
 
-Deleting ENM configurations one by one
+Batch executing async commands one by one
+from file. Next command starts after previous
+command's job's takes "COMPLETED" status
 
 """
 import enmscripting
@@ -23,31 +25,38 @@ def get_output(term, cmd):
     result = term.execute(cmd)
     if result.is_command_result_available():
         output = result.get_output()
-        print output
+        for out_str in output:
+            print out_str
         return output
 
 
-def get_job_status(term, job):
-    cmd = "config delete --status --job %s" % job
+def wait_for_completed_status(term, job, cm_pref):
+    cmd = "%s --status --job %s" % (cm_pref, job)
     while True:
         output = get_output(term, cmd)
-        if "COMPLETED" in output[2].split("\t"):
+        if "COMPLETED" in output[2].split("\t") \
+                or "Error" in output[1].split():
             break
         else:
             time.sleep(60)
+
+
+def print_red(str_to_print):
+    print "%s%s%s%s%s" % ("\x1B[", "31;40m", str_to_print, "\x1B[", "0m")
 
 
 def main():
     try:
         session = enmscripting.open()
         terminal = session.terminal()
-        for cfg_name in read_file("cfgs.txt"):
-            cmd = "config delete %s" % cfg_name
-            output = get_output(terminal, cmd)
-            jobid = output[-1].split()[-1]
-            get_job_status(terminal, jobid)
-            cfg_str = "Config %s has been deleted!" % cfg_name
-            print "%s%s%s%s%s" % ("\x1B[", "31;40m", cfg_str, "\x1B[", "0m")
+        for full_cmd in read_file("cfgs.txt"):
+            cmd_prefix = " ".join(full_cmd.split()[0:2])
+            output = get_output(terminal, full_cmd)
+            if "Error" not in " ".join(output).split():
+                jobid = output[-1].split()[-1]
+                wait_for_completed_status(terminal, jobid, cmd_prefix)
+                cfg_str = "Command <%s> has been executed!" % full_cmd
+                print_red(cfg_str)
     except Exception as e:
         print e
     finally:
